5cd91cbd789dc49b89469b9428699a94f9cea7de
enockabere <maebaenock95@gmail>
2021-09-13 01:45:54 +0300

    craete a logout route

--- profiles/views.py
+++ profiles/views.py
@@ -5,8 +5,8 @@ from django.contrib.auth.decorators import login_required
 
 # Create your views here.
 def index(request):
-    return render(request, 'home.html')
+    return render(request, 'index.html')
 
-# @login_required(login_url='accounts/login/')
+@login_required(login_url='accounts/login/')
 def home(request):
     return render(request,'profile/account.html')

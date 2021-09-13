7a8fcde1d2948ac913de9397de12913d1b30f968
enockabere <maebaenock95@gmail>
2021-09-13 10:43:20 +0300

    add profile model

--- profiles/models.py
+++ profiles/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
 # Create your models here.
+class Profile(models.Model):
+   dp = models.ImageField(null=False,blank=False)
+   bio = models.TextField()
+   
+   def __str__(self):
+       return self.bio 
+class Image(models.Model):
+    image = models.ImageField(null=False,blank=False)
+    description = models.TextField()
+    comment = models.TextField()
+    likes = models.IntegerField()
+    profile = models.ForeignKey(Profile,on_delete=models.CASCADE)
+    
+    def __str__(self):
+        return self.description
+
+    

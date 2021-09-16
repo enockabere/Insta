c9267d68521ac8edbcc3bd58f07ecfedddaeac5e
enockabere <maebaenock95@gmail>
2021-09-16 23:40:03 +0300

    create login template

--- profiles/views.py
+++ profiles/views.py
@@ -8,6 +8,8 @@ from . forms import NewUserForm
 from django.contrib.auth import login
 from django.contrib import messages
 from django.contrib.auth import get_user_model
+from django.contrib.auth import login, authenticate 
+from django.contrib.auth.forms import AuthenticationForm 
 
 User = get_user_model()
 
@@ -69,6 +71,25 @@ def register_request(request):
 		messages.error(request, "Unsuccessful registration. Invalid information.")
 	form = NewUserForm()
 	return render (request=request, template_name="main/register.html", context={"register_form":form})
+
+def login_request(request):
+	if request.method == "POST":
+		form = AuthenticationForm(request, data=request.POST)
+		if form.is_valid():
+			username = form.cleaned_data.get('username')
+			password = form.cleaned_data.get('password')
+			user = authenticate(username=username, password=password)
+			if user is not None:
+				login(request, user)
+				messages.info(request, f"You are now logged in as {username}.")
+				return redirect("home")
+			else:
+				messages.error(request,"Invalid username or password.")
+		else:
+			messages.error(request,"Invalid username or password.")
+	form = AuthenticationForm()
+	return render(request=request, template_name="main/login.html", context={"login_form":form})
+
 
 # def search_user(request):
 #     if 'search' in request.GET and request.GET["search"]:
